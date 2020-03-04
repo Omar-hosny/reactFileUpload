afe29d2691069f754f301d6f4461d7bd1534cf54
omar hosny <omarhosnystar@gmail.com>
2020-03-04 06:59:10 +0200

    reactFileUpload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import FileUpload from "./components/FileUpload";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react" /> React File upload
+      </h4>
+      <FileUpload />
     </div>
   );
-}
+};
 
 export default App;
